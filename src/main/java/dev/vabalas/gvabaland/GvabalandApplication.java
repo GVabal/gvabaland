dfed05b4bb4f77383b473ead583b843083da993b
Gabrielius Vabalas <gvabal@gmail.com>
2022-10-14 20:47:06 +0300

    Initial commit

---
+++ src/main/java/dev/vabalas/gvabaland/GvabalandApplication.java
@@ -0,0 +1,13 @@
+package dev.vabalas.gvabaland;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GvabalandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GvabalandApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/vabalas/gvabaland/GvabalandApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.vabalas.gvabaland;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GvabalandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
